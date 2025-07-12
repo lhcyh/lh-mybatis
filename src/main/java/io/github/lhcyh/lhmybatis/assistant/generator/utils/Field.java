39c507ab5af6f451227224c61637c84a3c549265
cyh <17802011020@163.com>
2025-07-12 16:31:14 +0800

    空格问题

--- src/main/java/io/github/lhcyh/lhmybatis/assistant/generator/utils/Field.java
+++ src/main/java/io/github/lhcyh/lhmybatis/assistant/generator/utils/Field.java
@@ -5,30 +5,6 @@ import io.github.lhcyh.lhmybatis.assistant.generator.enums.Type;
 import java.io.Serializable;
 
 public class Field implements Serializable {
-//    public enum Type{
-//        VARCHAR("varchar"),
-//        INT("int"),
-//        DATETIME("datetime"),
-//        FLOAT("float"),
-//        BIT("bit"),
-//        ENUM("enum");
-//        private String value;
-//        Type(String value){
-//            this.value=value;
-//        }
-//        public String getValue(){
-//            return this.value;
-//        }
-//
-//        public static Type getTypeByValue(String value){
-//            for(Type type: Type.values()){
-//                if(type.value.equals(value)){
-//                    return type;
-//                }
-//            }
-//            return null;
-//        }
-//    }
     private String name;
     private Type type;
     private Integer size;
@@ -37,21 +13,6 @@ public class Field implements Serializable {
     private boolean isPrimaryKey;
 
     public String getJavaType(){
-//        switch (type){
-//            case VARCHAR:
-//                return "String";
-//            case INT:
-//                return "Integer";
-//            case BIT:
-//                return "Boolean";
-//            case FLOAT:
-//                return "Float";
-//            case DATETIME:
-//                return "Date";
-//            case ENUM:
-//                return Utils.underscoreToCamel(name,true);
-//        }
-//        return null;
         if(type==Type.ENUM){
             return Utils.underscoreToCamel(name,true);
         }else {
@@ -61,7 +22,7 @@ public class Field implements Serializable {
 
     public String getIfTag(String fill){
         String tag=fill+"<if test=\""+ Utils.underscoreToCamel(name,false)+"!=null\">\n"+
-                fill+"   `"+name+"`=#{"+ Utils.underscoreToCamel(name,false)+"},\n"+
+                fill+"    `"+name+"`=#{"+ Utils.underscoreToCamel(name,false)+"},\n"+
                 fill+"</if>";
         return tag;
     }
@@ -76,32 +37,32 @@ public class Field implements Serializable {
 
     public String getJavaGet(String fill){
         String head=" get";
-        if(type== Type.BIT){
-            head=" is";
-        }
+//        if(type== Type.BIT){
+//            head=" is";
+//        }
         String javaGet=fill+"public "+getJavaType()+head+ Utils.underscoreToCamel(name,true)+"(){\n";
-        javaGet+=fill+"  "+"return "+ Utils.underscoreToCamel(name,false)+";\n";
+        javaGet+=fill+"    "+"return "+ Utils.underscoreToCamel(name,false)+";\n";
         javaGet+=fill+"}";
         return javaGet;
     }
 
     public String getJavaSet(String fill){
         String javaSet=fill+"public void set"+ Utils.underscoreToCamel(name,true)+"("+getJavaType()+" "+ Utils.underscoreToCamel(name,false)+"){\n";
-        javaSet+=fill+"  "+"this."+ Utils.underscoreToCamel(name,false)+"="+ Utils.underscoreToCamel(name,false)+";\n";
+        javaSet+=fill+"    "+"this."+ Utils.underscoreToCamel(name,false)+"="+ Utils.underscoreToCamel(name,false)+";\n";
         javaSet+=fill+"}";
         return javaSet;
     }
 
     public String getJavaListGet(String fill){
         String javaListGet=fill+"public List<"+getJavaType()+"> get"+ Utils.underscoreToCamel(name,true)+"List(){\n";
-        javaListGet+=fill+"  "+"return "+ Utils.underscoreToCamel(name,false)+"List;\n";
+        javaListGet+=fill+"    "+"return "+ Utils.underscoreToCamel(name,false)+"List;\n";
         javaListGet+=fill+"}";
         return javaListGet;
     }
 
     public String getJavaListSet(String fill){
         String javaListSet=fill+"public void set"+ Utils.underscoreToCamel(name,true)+"List(List<"+getJavaType()+"> "+ Utils.underscoreToCamel(name,false)+"){\n";
-        javaListSet+=fill+"  "+"this."+ Utils.underscoreToCamel(name,false)+"List="+ Utils.underscoreToCamel(name,false)+"List;\n";
+        javaListSet+=fill+"    "+"this."+ Utils.underscoreToCamel(name,false)+"List="+ Utils.underscoreToCamel(name,false)+"List;\n";
         javaListSet+=fill+"}";
         return javaListSet;
     }
